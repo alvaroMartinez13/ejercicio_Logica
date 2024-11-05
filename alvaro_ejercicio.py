f6050ed80eb966c7388e92142402c62d954de422
Alvaro Andres Martinez Alcina <166070500+alvaroMartinez13@users.noreply.github.com>
2024-11-05 15:49:46 -0500

    Update alvaro_ejercicio.py
    By @alvaroMartinez13

--- alvaro_ejercicio.py
+++ alvaro_ejercicio.py
@@ -1,21 +1,18 @@
 #Escribir un programa que imprima los 10 primeros números primos comenzando en 2 e imprima también sus respectivos cubos.
 
-numeros = [1,2,3,4,5,6,7,8,9,10]
-
-def primo(lista):
-    for i in range(2,len(lista)+1,2):
-            print(i)
+def primo():
+    for i in range(1, 30):
+        print("Número primo: ",i)
+        cubo(i)
     
 
-def cubo(lista):
-    for i in range(0,len(lista)):
-     print("El cubo de ",lista[i]," es: ",lista[i]**3)
+def cubo(num):
 
-def main():
-    print("---Primos---")    
-    primo(numeros)
+     print("Cubo del primo: ",num**3,"\n")
 
-    print("\n---Cubos---")
-    cubo(numeros)
+def main(): 
+    print()  
+    primo()
+
 
 main()
